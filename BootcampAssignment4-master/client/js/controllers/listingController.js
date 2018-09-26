82143b42c22227e4898d3f62c15b58a175541cf9
Jason B <jasonb3194@yahoo.com>
2018-09-25 23:57:37 -0400

    seeing if it works now

--- BootcampAssignment4-master/client/js/controllers/listingController.js
+++ BootcampAssignment4-master/client/js/controllers/listingController.js
@@ -10,17 +10,23 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
     $scope.detailedInfo = undefined;
 
     $scope.addListing = function() {
-	  /**TODO 
-	  *Save the article using the Listings factory. If the object is successfully 
-	  saved redirect back to the list page. Otherwise, display the error
-	 */
+	
+      Listings.create($scope.newListing).then(function(response) {
+
+        $scope.listings.push($scope.newListing);
+
+        $scope.newListing = {};
+      }, function(error) {
+        console.log('error', error);
+      });
     };
 
     $scope.deleteListing = function(index) {
-	   /**TODO
-        Delete the article using the Listings factory. If the removal is successful, 
-		navigate back to 'listing.list'. Otherwise, display the error. 
-       */
+
+      var temp = $scope.listings[code];
+      Listings.delete(temp._id);
+      $scope.listings.splice(code, 1);	
+      
     };
 
     $scope.showDetails = function(index) {
